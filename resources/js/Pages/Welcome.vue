36bcb7d4e6b2f9277e46bef150c26efef03b2840
Blackmokujingal <bea800.w98@gmail.com>
2023-03-06 00:03:01 +0400

    Finished the initialization + started the project -Added Vue.js via Vite -Added TailwindCSS -Added the side naviagation layout

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+import SocialAppLayout from '@/Layouts/SocialAppLayout.vue';
+</script>
+
+<template>
+    <Head title="Welcome" />
+
+    <SocialAppLayout>
+        <div class="bg-red-900">Welcome to you </div>
+    </SocialAppLayout>    
+</template>
+
